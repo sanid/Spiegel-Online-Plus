dac1acfc745d962333fed75c0de6b72dfa28681c
Sanid <hi@sanid.me>
2016-10-08 01:36:56 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         Spiegel Plus
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Spiegel Plus "Verschlüsselung" disable
+// @author       Sanid
+// @match        http://www.spiegel.de/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    SPLaterpayCallbackHasAccess();
+})();
